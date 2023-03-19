d3cec2b4939f881fe98eb90952c16b93ca59bf40
yaumatth <yaumatth>
2023-03-18 18:12:53 -0700

    Changed monthly record graph

--- src/apps/Monthly-Record-Graph.py
+++ src/apps/Monthly-Record-Graph.py
@@ -88,6 +88,69 @@ us_state_to_abbrev = {
 murder["State_Code"] = murder["State"].replace(us_state_to_abbrev,
                                                inplace=False)
 
+
+replace_dict_relationship = {"Wife": "Family",
+                             "Son": "Family",
+                             "Sister": "Family",
+                             "Common-Law Husband": "Family",
+                             "Common-Law Wife": "Family",
+                             "Mother": "Family",
+                             "Father": "Family",
+                             "Stepmother": "Family",
+                             "Stepdaughter": "Family",
+                             "Stepson": "Family",
+                             "Stepfather": "Family",
+                             "Brother": "Family",
+                             "In-Law": "Family",
+                             "Husband": "Family",
+                             "Common-Law Family": "Family",
+                             "Daughter": "Family",
+
+                             "Employee": "Acquaintance",
+                             "Neighbor": "Acquaintance",
+                             "Employer": "Acquaintance",
+
+
+
+                             "Ex-Husband": "Unknown",
+                             "Ex-Wife": "Unknown",
+                             "Ex-Family": "Unknown",
+
+                             "Boyfriend/Girlfriend": "Dating partner",
+                             "Boyfriend": "Dating partner",
+                             "Girlfriend": "Dating partner"
+
+                            }
+
+
+
+for key, value in replace_dict_relationship.items():
+    murder["Relationship"] = murder["Relationship"].str.replace(key, value)
+
+
+replace_dict_weapon = {"Handgun": "Gun",
+                       "Rifle": "Gun",
+                       "Shotgun": "Gun",
+                       "Gun": "Gun",
+                       "Firearm": "Gun",
+                       "Knife": "Melee weapon",
+                       "Blunt Object": "Melee weapon",
+                       "Suffocation": "Unarmed",
+                       "Strangulation": "Unarmed",
+                       "Fire": "Other",
+                       "Drugs": "Other",
+                       "Drowning": "Other",
+                       "Explosives": "Other",
+                       "Fall": "Other",
+                       "Poison": "Other",
+                      }
+
+
+
+
+for key, value in replace_dict_weapon.items():
+    murder["Weapon"] = murder["Weapon"].str.replace(key, value)
+
 # ------------------------------------------------------ STYLE ------------------------------------------------------
 CONTENT_STYLE = {
     "margin-left": "23rem",
@@ -121,24 +184,24 @@ layout = dbc.Container(
                     [
                         dbc.CardBody([
                             html.Br(),
-                            html.Label("Crime Type: "),
+                            html.Label("Perpetrator Gender: "),
                             dcc.Dropdown(
-                                id="input_crime_type",
-                                value="Murder or Manslaughter",
+                                id="input_perpetrator_gender",
+                                value="Male",
                                 multi=False,
                                 clearable=False,
                                 style={"font-size": "smaller"},
                                 options=[{
                                     "label":
-                                    crime_type,
+                                    perpetrator_gender,
                                     "value":
-                                    crime_type
-                                } for crime_type in list(
-                                    murder["Crime_Type"].dropna().unique())]),
+                                    perpetrator_gender
+                                } for perpetrator_gender in list(
+                                    murder["Perpetrator_Sex"].dropna().unique())]),
                             html.Br(),
                             html.Label("State: "),
                             dcc.Dropdown(id="input_state",
-                                         value="Alaska",
+                                         value="California",
                                          multi=False,
                                          clearable=True,
                                          style={"font-size": "smaller"},
@@ -152,7 +215,7 @@ layout = dbc.Container(
                             html.Label("Weapon Type: "),
                             dcc.Dropdown(
                                 id="input_weapon",
-                                value="Blunt Object",
+                                value="Gun",
                                 multi=False,
                                 clearable=True,
                                 style={"font-size": "smaller"},
@@ -200,12 +263,12 @@ layout = dbc.Container(
 
 
 # ------------------------------------------------------ CALLBACK ------------------------------------------------------
-@callback(Output("rose_plot", "figure"), Input("input_crime_type", "value"),
+@callback(Output("rose_plot", "figure"), Input("input_perpetrator_gender", "value"),
           Input("input_state", "value"), Input("input_weapon", "value"),
           Input("input_relationship", "value"))
-def plot_rose(input_crime_type, input_state, input_weapon, input_relationship):
+def plot_rose(input_perpetrator_gender, input_state, input_weapon, input_relationship):
     #filter
-    murder_selected = murder.loc[(murder["Crime_Type"] == input_crime_type)
+    murder_selected = murder.loc[(murder["Perpetrator_Sex"] == input_perpetrator_gender)
                                  & (murder["State"] == input_state)
                                  & (murder["Weapon"] == input_weapon) &
                                  (murder["Relationship"]
